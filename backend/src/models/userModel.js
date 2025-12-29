903d011636aae0dacd755ba5e1a97db36661b95a
Ravindra Jadhav <jadhaor181@gmail.com>
2025-12-30 00:37:42 +0530

    created user model with databae queries.

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,64 @@
+const pool = require('../config/db');
+
+const userModel = {
+
+    //Create a New user
+    createUser: async (email, passwordHash, fullName, role = 'user') => {
+        const query = `
+           INSERT INTO users(email, password_hash, full_name, role, status)
+           VALUES ($1, $2, $3, $4, 'active)
+           RETURNING id, email, full_name, role, status, created_at
+        `;
+        const values = [email, passwordHash, fullName, role];
+
+        try{
+            const result = await pool.query(query, values);
+            return result.rows[0];
+        }
+        catch (error) {
+            throw error;
+        }
+    },
+
+    //Find User by email
+    findByEmail : async(email) => {
+    const query = 'SELECT * FROM users WHERE email = $1';
+        try{
+            const result = await pool.query(query, [email]);
+            return result.row[0] || null;
+        }
+        catch (error) {
+            throw error;
+        }
+    },
+
+    //Find user by Id
+    findById : async(id) => {
+        const query =`
+        SELECT id, email, full_name, role, status, created_at, updated_at, last_login
+        FROM users
+        WHERE id = $1
+        `;
+        try {
+            return result.rows[0] || null;
+        } 
+        catch (error) {
+            throw error;
+        }
+    },
+
+    //Check if email exists
+    emailExists: async (email) => {
+        const query = 'SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)';
+
+        try {
+            const result = await pool.query(query, [email]);
+            return result.rows[0].exists;
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+};
+
+module.exports = userModel;
